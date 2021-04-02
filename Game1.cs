63347c35f3a64c7918bc621de0a2d37df7988b64
19klowe0 <kiralowe98@gmail.com>
2021-04-02 00:29:28 -0400

    Camera/Movement
    Gun/Camera now rotates and if you press space you get a distant view of the gun

--- Game1.cs
+++ Game1.cs
@@ -9,7 +9,15 @@ namespace SaloonShootout
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        Model gun;
+        //needed player information
+        Vector3 playerPos;
+        Model player;
+        Vector3 playerDir;
+        float playerRot;
+
+        //for different camera view for testing
+        Vector3 camOffset;
+
 
         public Game1()
         {
@@ -21,6 +29,10 @@ namespace SaloonShootout
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            playerPos = new Vector3(0, 0, 0); //(0,0,0)
+            playerRot = 0.0f;
+            camOffset = new Vector3(0, 6, 20);
+            playerDir = Vector3.Zero;
 
             base.Initialize();
         }
@@ -29,7 +41,7 @@ namespace SaloonShootout
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            gun = Content.Load<Model>("RevolverBeforeBake");
+            player = Content.Load<Model>("RevolverBeforeBake");
 
             // TODO: use this.Content to load your game content here
         }
@@ -40,6 +52,16 @@ namespace SaloonShootout
                 Exit();
 
             // TODO: Add your update logic here
+            //rotate to the Right
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                playerRot -= .25f;
+            }
+            //rotate to the left
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                playerRot += .25f;
+            }
 
             base.Update(gameTime);
         }
@@ -49,19 +71,49 @@ namespace SaloonShootout
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             Matrix proj = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(60),
-                                                                                  1,
-                                                                                  0.001f,
-                                                                                  100f);
+                                                        (float)_graphics.PreferredBackBufferWidth / _graphics.PreferredBackBufferHeight,
+                                                        .001f,
+                                                        100f);
 
-            Matrix view = Matrix.CreateLookAt(new Vector3(0, 6, 20),
-                                              new Vector3(0, 0, 0),
-                                              new Vector3(0, 1, 0));
+           //code for rotation
+            Matrix view = Matrix.CreateLookAt(
+                Vector3.Transform(camOffset, Matrix.CreateRotationY(playerRot)),
+                new Vector3(0, 0, 0),
+                Vector3.Up);
 
-            Matrix world = Matrix.CreateScale(0.045f) *
+            //in the distance camera 
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                view = Matrix.CreateLookAt(new Vector3(0 + playerPos.X, 0, 30),
+                                         new Vector3(playerPos.X, 0, 0),
+                                         Vector3.Up); //(0,1,0)
+            }
+
+
+            Matrix world =   Matrix.CreateRotationY(playerRot)
+                                              * Matrix.CreateTranslation(playerPos)
+                                              *Matrix.CreateScale(0.045f) *
                            Matrix.CreateRotationY(MathHelper.ToRadians(90)) *
                            Matrix.CreateTranslation(Vector3.Zero);
 
-            gun.Draw(world, view, proj);
+
+
+            //enable the lighting for the gun meshes
+            foreach (ModelMesh mesh in player.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = world;
+                    effect.View = view;
+                    effect.Projection = proj;
+
+                    effect.EnableDefaultLighting();
+                    effect.LightingEnabled = true;
+                }
+            }
+
+
+            player.Draw(world, view, proj);
 
             // TODO: Add your drawing code here
 
@@ -69,3 +121,4 @@ namespace SaloonShootout
         }
     }
 }
+
